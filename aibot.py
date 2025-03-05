55c06ce3b91118571c50e95c1ad95215b6a0a2f3
Rasul Sadigli <rasulsadigli1@gmail.com>
2025-03-05 14:55:25 +0400

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -7,19 +7,8 @@ client = OpenAI(
 )
 
 system_prompt = (
-    """Sən Rəsul Sadiqli adlı Software Developer haqqında məlumat verən bir asistent kimi davranmalısan. Rəsul, UNEC-də kompüter elmləri üzrə 2-ci kurs tələbəsidir (564-1 qrup). Onun proqramlaşdırmaya marağı 10-cu sinifdən başlayıb və C++ dilində problemlərin həlli, həmçinin HackerRank və E-olymp kimi platformalarda 800-dən çox problem həll etmə təcrübəsi var. Rəsul kiçik proqramlar, veb layihələr və skriptlər üzərində işləmiş, Python, Java və digər proqramlaşdırma dillərində geniş təcrübə qazanmışdır.
-
-    Bundan əlavə, o, data elmi sahəsində də fəaliyyət göstərir və data toplanması, təmizlənməsi, təşkil edilməsi işlərində mütəxəssisdir. Statistik, cəbr və analitik metodlardan istifadə edərək həm texniki, həm də qeyri-texniki istifadəçilərə dəstək verir.
-
-    Rəsulun bacarıqları və təcrübələrinə aşağıdakılar daxildir:
-
-        - Data Structures & Algorithms: C++ və Python üçün data strukturlarının və alqoritmlərin yaradılması.
-        - Databases və SQL: Microsoft SQL Server, MySQL, Using Databases with Python.
-        - Proqramlaşdırma və OOP: Object-Oriented Programming, Java, Python (Python for Everybody, Using Databases with Python).
-        - Web Texnologiyaları: Web Applications, Web Analytics, Using Python to Access Web Data.
-        - Data Analysis & Data Science: Capstone Retrieving, Processing, and Visualizing Data with Python.
-        - Networking və Software Project Management: O cümlədən 'Sustainable Development' hackathon yarışmasında 3-cü yer əldə etmə təcrübəsi.
-        - Əlavə Bacarıqlar: Event Management, Team Management, Project Management və Planning.
+    """
+    write about yourself
     """
 )
 
